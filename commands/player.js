a02c865663a358ed73a3b11b52aca13cd91e59b1
Void <thevoidf@gmail.com>
2019-07-14 00:26:07 +0400

    Refactored player

--- commands/player.js
+++ commands/player.js
@@ -4,7 +4,7 @@ const queue = new Map();
 
 module.exports = async props => {
 	const { message, command, commandArgs } = props;
-
+	const { channel: textChannel, member: { voiceChannel } } = message;
 	const serverQueue = queue.get(message.guild.id);
 
 	const playTrack = async (guild, track) => {
@@ -20,20 +20,11 @@ module.exports = async props => {
 			serverQueue.tracks.shift();
 			playTrack(guild, serverQueue.tracks[0]);
 		});
-	};
-
-	if (command === 'leave') {
-		if (!message.member.voiceChannel)
-			return message.reply('You need to be in voice channel niga');
-		message.member.voiceChannel.leave();
 	}
 
 	if (command === 'play') {
-		const textChannel = message.channel;
-		const voiceChannel = message.member.voiceChannel;
-
 		if (!voiceChannel)
-			return message.reply('You need to be in voice channel niga');
+			return message.reply('You need to be in voice channel');
 
 		const trackInfo = await ytdl.getInfo(commandArgs[0]);
 		const track = {
@@ -45,9 +36,7 @@ module.exports = async props => {
 			const guildQueue = {
 				textChannel,
 				voiceChannel,
-				tracks: [],
-				volume: 5,
-				playing: true
+				tracks: []
 			}
 
 			queue.set(message.guild.id, guildQueue);
@@ -55,15 +44,15 @@ module.exports = async props => {
 
 			const connection = await voiceChannel.join();
 			guildQueue.connection = connection;
-			playTrack(message.guild, guildQueue.tracks[0]);
-		} else {
-			serverQueue.tracks.push(track);
-			textChannel.send(`${track.title} added to queue`);
+			return playTrack(message.guild, guildQueue.tracks[0]);
 		}
+
+		serverQueue.tracks.push(track);
+		textChannel.send(`${track.title} added to queue`);
 	}
 
 	if (command === 'skip') {
-		if (queue.length === 0)
+		if (!serverQueue)
 			return message.channel.send('No tracks to skip');
 		serverQueue.connection.dispatcher.end();
 	}
@@ -74,14 +63,20 @@ module.exports = async props => {
 	}
 
 	if (command === 'pause') {
-		if (!message.member.voiceChannel)
-			return message.reply('You need to be in voice channel niga');
+		if (!voiceChannel)
+			return message.reply('You need to be in voice channel');
 		serverQueue.connection.dispatcher.pause();
 	}
 
 	if (command === 'resume') {
-		if (!message.member.voiceChannel)
-			return message.reply('You need to be in voice channel niga');
+		if (!voiceChannel)
+			return message.reply('You need to be in voice channel');
 		serverQueue.connection.dispatcher.resume();
+	}
+
+	if (command === 'leave') {
+		if (!voiceChannel)
+			return message.reply('You need to be in voice channel');
+		voiceChannel.leave();
 	}
 }
