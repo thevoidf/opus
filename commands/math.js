6a484c1c8ac2d97c7dbe0465cf733e3a80ac8249
Void <thevoidf@gmail.com>
2019-07-17 23:21:01 +0400

    Added math command

---
+++ commands/math.js
@@ -0,0 +1,32 @@
+const math = module.exports = exports = {};
+
+const context = `
+'use strict';
+
+const pow = Math.pow;
+const cos = Math.cos;
+const sin = Math.sin;
+const pi = Math.PI;
+`;
+const regex = /[-\\*/,()\s+\d]|\bpow\b|\bcos\b|\bsin\b|\bpi\b/g;
+
+math.calc = ({ message, command, args }) => {
+	const { channel } = message;
+
+	if (args.length === 0)
+		return channel.send('Plese do some math');
+
+	const math = args.join('');
+	const match = math.match(regex);
+
+	if (!match)
+		return channel.send('Invalid input');
+
+	if (match.join('') !== math)
+		return channel.send('Invalid input');
+
+	const calculation = context + math;
+
+	const output = eval(calculation);
+	channel.send(':nerd: ' + output);
+}
